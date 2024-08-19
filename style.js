1c0918dd0681c5c548165fbf06c9bbe79392c60c
souravkaushik-dev <souravkaushik.dev@gmail.com>
2024-08-19 22:21:16 +0530

    Delete style.js

--- style.js
+++
@@ -1,8 +0,0 @@
-const WIDTH = 33.33;
-
-const btnContainer = document.querySelector(".container");
-
-function slideBg(n) {
-  const bgOffset = WIDTH * n;
-  btnContainer.style.setProperty("--bg-offset", `${bgOffset}%`);
-}
