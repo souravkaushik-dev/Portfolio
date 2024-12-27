430184d483d6122b3bbfab07d984b21b58ef106b
dotstudios <souravkaushik.dev@gmail.com>
2024-12-27 23:06:16 +0530

    Add files via upload

---
+++ donate.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html><html lang="en" class="donation-banner-iframe"><head><title>Donate</title><style>body{margin:0}</style>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      <script src="js/jquery-1.10.2.min.js"></script>
+      <script src="js/polyfill.min.js"></script>
+      <script src="js/athena.js"></script>
+          <script src="js/donation-banner.min.js" type="text/javascript"></script>
+    </head>
+    <body class="js-ia-iframe" style="margin:0">
+          <link href="css/banner.css" rel="stylesheet" type="text/css">
+    <link href="css/chevron.css" rel="stylesheet" type="text/css">
+
+    <header id="donate_banner" class="row js-donate-banner template-chevron background-default chevron-image-white goldbarvisible chevron-color-#194880 formdesign-form" itemscope="" itemtype="http://schema.org/WPAdBlock" data-donationsourcedata="ctx=wb;uid=5a82b84ae20b9e8aae1354463f6dee41;ref=https%3A%2F%2Fweb.archive.org%2Fweb%2F20230329192447%2Fhttps%3A%2F%2Fwww.thecraftsmen.tech%2F" data-exp="20241220DefaultEOYBannerV3" data-variant="20Dec2024EOYDefaultWB" data-platform="wb" data-baseurl="https://archive.org" data-bannertemplate="chevron" data-bannerbackground="default" data-snowfall="off" data-selectedtextcolor="#ffffff" data-selectedfillcolor="#194880" data-paymentoptionfillcolor="" data-paymentoptiontextcolor="" data-formbgcolor="#ffffff" data-formtextcolor="#000000" data-donatepageselectedtextcolor="#ffffff" data-donatepageselectedfillcolor="#194880" data-variantdollaramounts="[5, 15.58, 50]" data-variantamountlayout="multi-line" data-variantfrequencymode="checkbox" data-debugmode="false" style="--donationBannerFormTextColor: #000000;">
+      <button id="donate-close-button" class="donate-close" type="button" aria-label="Close banner">
+        <span class="sr-only">Close banner</span>
+        <img src="images/close-circle-dark.svg" id="banner-close-image-dark">
+        <img src="images/close-circle-white.svg" id="banner-close-image-white">
+      </button>
+      <div id="banner-body-container" style="background-color: #ffffff;">
+        <div id="banner-body-left-container" class="show-separator" style="--donationBannerChevronBG: #194880;">
+                      <p class="donate-notification">
+              <strong>5 Days Left:</strong> The year is almost over—help us meet our 2024 goal!            </p>
+                    <div class="donate-body">
+                                      <div id="chevron-image-container" style="background-color: #ffffff;"></div>
+            
+            <div class="main-content">
+              <div class="appeal-text" style="--donationBannerAppealTextColor: #ffffff;">
+                <div class="desktop-body-content"><b>Can You Chip In?</b><br><br>Please don't scroll past this—the Wayback Machine is fighting for universal access to quality information. The Internet Archive, which runs this project, relies on online donations averaging $15.58 to help us keep the record straight. We'd be deeply grateful if you'd join the one in a thousand users that support us financially.<br><br>We understand that not everyone can donate right now, but if you can afford to contribute this Friday, we promise it will be put to good use. Our resources are crucial for knowledge lovers everywhere—so if you find all these bits and bytes useful, <b>please pitch in.</b></div>
+                <div class="mobile-body-content"><b>Can You Chip In?</b> Please don't scroll past this. The nonprofit which runs this project relies on online donations averaging $15.58. If you find our library useful, <b>please pitch in.</b></div>
+              </div>
+
+                          </div>
+            <div id="donate-body-background-layer2" class="supplemental-background-layer"></div>
+            <div id="donate-body-background-layer1" class="supplemental-background-layer"></div>
+          </div>
+        </div>
+
+        <div class="donate-form js-donate-form" itemscope="" itemtype="http://schema.org/DonateAction">
+          <meta itemprop="recipient" content="Internet Archive">
+          <meta itemprop="actionStatus" content="PotentialActionStatus">
+          <div class="white-box">
+                                <div class="donation-form-container">
+        <donation-form-edit-donation amountoptions="[5, 15.58, 50]" defaultselectedamount="15.58" amountselectionlayout="multi-line" frequencyselectionmode="checkbox" stepnumbermode="hidenumbers" style="--paymentButtonSelectedFontColor: #ffffff;--paymentButtonSelectedColor: #194880;--formSectionTextColor: #000000;">
+        </donation-form-edit-donation>
+
+        <div class="cta">
+          <button class="cta-button" id="continue-button" style="color: #ffffff;background-color: #24765e;">
+            Continue
+          </button>
+                <div class="donate-later-container">
+        <a id="donate_later" href="#donate_dropdown" data-source-context="WB" data-event-click-tracking="DonateBannerWB|MaybeLaterLink" data-event-tracking-options="{ "service": "ao_no_sampling" }">Remind Me</a>
+        <div class="donate-later-wrap">
+          <div class="donate-later-dropdown js-donate-later-dropdown" id="donate_dropdown">
+            <button id="close_donate_dropdown" class="donate-close" type="button" aria-label="Close banner"><span class="iconochive-remove-circle"></span>
+            </button>
+            <form id="donate_reminder_form" data-base-url="https://archive.org">
+              <h5>Send me an email reminder</h5>
+              <label for="email">Email address (required)</label>
+              <input type="email" id="email" name="email" value="">
+              <div class="error-email">Please enter a valid email address.</div>
+              <label for="first-name">First name</label>
+              <input type="text" id="first-name" name="first_name" value="">
+              <label for="last-name">Last name</label>
+              <input type="text" id="last-name" name="last_name" value="">
+              <button type="submit">Submit</button>
+              <p>
+                By submitting, you agree to receive donor-related emails from the Internet Archive.
+                Your privacy is important to us. We do not sell or trade your information with anyone.
+              </p>
+            </form>
+            <div class="reminder-success hidden" id="donate_reminder_success" aria-live="polite">
+            </div>
+          </div>
+        </div>
+      </div>
+            </div>
+      </div>
+                          </div> <!--/.white-box -->
+          <div class="donate-form-chevrons" style="--donationBannerChevronBG: #194880;"></div>
+        </div> <!-- .donate-form -->
+      </div>
+    </header>
+          
+  
+</body></html>
---
+++ js/904b24cd.js
@@ -0,0 +1,34 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+!function(){var n;(n=window.cookiehub||{}).hasInitialised||(n.Popup=function(){function n(){this.initialise.apply(this,arguments)}return n.prototype.initialise=function(n){var i=document.createElement("div");i.style.cssText="position: fixed;bottom: 10px;opacity: 0.8;background: #fff;font-size: 12px;border: 1px solid #ccc;color: #000;right: 10px;z-index: 99999;padding: 5px 9px;",i.innerHTML="CookieHub - This website has exceeded the included session limit or the service has been suspended",document.body.appendChild(i)},n}(),n.initialise=function(i,o,e){o||(o=function(){}),e||(e=function(){}),new n.Popup(i)},n.load=function(i,o,e){o||(o=function(){}),e||(e=function(){}),new n.Popup(i)},n.hasInitialised=!0,n.hasConsented=function(n){return!1},n.hasAnswered=function(){return!1},n.allowAll=function(){},n.denyAll=function(){},n.openDialog=function(){},n.closeDialog=function(){},n.openSettings=function(){},n.closeSettings=function(){},window.cookiehub=n)}();
+
+}
+/*
+     FILE ARCHIVED ON 07:15:48 May 08, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 09:50:19 Dec 27, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.629
+  exclusion.robots: 0.022
+  exclusion.robots.policy: 0.01
+  esindex: 0.013
+  cdx.remote: 89.892
+  LoadShardBlock: 80.094 (3)
+  PetaboxLoader3.datanode: 85.221 (4)
+  load_resource: 15.998
+*/
